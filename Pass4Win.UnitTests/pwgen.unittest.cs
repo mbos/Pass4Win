0e693584c83e4072099a2fc75723e025afa218c2
mikebos <mikebos@mik3.com>
2015-10-11 20:42:11 +0200

    First Unittest for pwgen

---
+++ Pass4Win.UnitTests/pwgen.unittest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Pass4Win
+{
+    [TestFixture]
+    public class PWGenUnitTests
+    {
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            // nothing to setup
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            // nothing to destroy
+        }
+
+        [Test]
+        public void TestHowManyChars()
+        {
+            string result = pwgen.Generate(10);
+            Assert.AreEqual(result.Length, 10);
+
+            string result2 = pwgen.Generate(80);
+            Assert.AreEqual(result2.Length, 80);
+        }
+    }
+}
