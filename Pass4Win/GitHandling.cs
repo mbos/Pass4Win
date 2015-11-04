c03de0f75477f44c05ab6332030e7f2f5be8e69b
mikebos <mikebos@mik3.com>
2015-11-04 20:59:25 +0100

    Added missing files

---
+++ Pass4Win/GitHandling.cs
@@ -0,0 +1,353 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="" file="GitHandling.cs">
+//   
+// </copyright>
+// <summary>
+//   Class to interface with the Git Repo
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+namespace Pass4Win
+{
+    using System;
+    using System.Linq;
+    using System.Net.Sockets;
+
+    using LibGit2Sharp;
+
+    /// <summary>
+    ///     Class to interface with the Git Repo
+    /// </summary>
+    public class GitHandling : IDisposable
+    {
+        /// <summary>
+        ///     The repo remote location object.
+        /// </summary>
+        private Repository gitRepo;
+
+        /// <summary>
+        ///     The host.
+        /// </summary>
+        private readonly string host;
+
+        /// <summary>
+        ///     The repo location.
+        /// </summary>
+        private readonly string repoLocation;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GitHandling"/> class.
+        /// </summary>
+        /// <param name="repoLocation">
+        /// The repo location.
+        /// </param>
+        /// <param name="host">
+        /// The host.
+        /// </param>
+        public GitHandling(string repoLocation, string host)
+        {
+            this.repoLocation = repoLocation;
+            this.host = host;
+        }
+
+        /// <summary>
+        ///     The dispose function of this class.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Checks if repo is valid.
+        /// </summary>
+        /// <param name="location">
+        /// The location.
+        /// </param>
+        /// <returns>
+        /// Sucess <see cref="bool"/>.
+        /// </returns>
+        public static bool IsValid(string location)
+        {
+            return Repository.IsValid(location);
+        }
+
+        /// <summary>
+        /// The check connection.
+        /// </summary>
+        /// <param name="hostName">
+        /// The host name.
+        /// </param>
+        /// <returns>
+        /// Succes or failure <see cref="bool"/>.
+        /// </returns>
+        public static bool CheckConnection(string hostName)
+        {
+            Uri hostTest;
+            if (Uri.TryCreate(hostName, UriKind.Absolute, out hostTest))
+            {
+                var client = new TcpClient();
+                var client2 = new TcpClient();
+                try
+                {
+                    var gitAlive = true;
+                    var httpsAlive = true;
+
+                    // Check Git connection
+                    var result = client.BeginConnect(hostTest.Authority, 9418, null, null);
+                    result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+                    if (!client.Connected)
+                    {
+                        client.Close();
+                        gitAlive = false;
+                    }
+
+                    // Check HTTPS
+                    var result2 = client2.BeginConnect(hostTest.Authority, 443, null, null);
+                    result2.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+                    if (!client2.Connected)
+                    {
+                        client2.Close();
+                        httpsAlive = false;
+                    }
+
+                    if (gitAlive || httpsAlive)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            // fail
+            return false;
+        }
+
+        /// <summary>
+        ///     The connect to repo.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="bool" />.
+        /// </returns>
+        public bool ConnectToRepo()
+        {
+            if (CheckConnection(this.host) && Repository.IsValid(this.repoLocation))
+            {
+                try
+                {
+                    this.gitRepo = new Repository(this.repoLocation);
+
+                    // Making sure core.autocrlf = true
+                    this.gitRepo.Config.Set("core.autocrlf", true);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// The git clone.
+        /// </summary>
+        /// <param name="username">
+        /// The username.
+        /// </param>
+        /// <param name="password">
+        /// The password.
+        /// </param>
+        /// <returns>
+        /// return boolean <see cref="bool"/>.
+        /// </returns>
+        public bool GitClone(string username, string password)
+        {
+            try
+            {
+                Repository.Clone(
+                    this.host, 
+                    this.repoLocation, 
+                    new CloneOptions
+                        {
+                            CredentialsProvider =
+                                (url, user, cred) =>
+                                new UsernamePasswordCredentials
+                                    {
+                                        Username = username, 
+                                        Password = password
+                                    }
+                        });
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Get's the latest and greatest from remote
+        /// </summary>
+        /// <param name="gitUser">
+        /// The Git User.
+        /// </param>
+        /// <param name="gitPass">
+        /// The Git Pass.
+        /// </param>
+        /// <returns>
+        /// A boolean that signals success
+        /// </returns>
+        public bool Fetch(string gitUser, string gitPass)
+        {
+            try
+            {
+                var signature = new Signature(
+                    "pass4win", 
+                    "pull@pass4win.com", 
+                    new DateTimeOffset(2011, 06, 16, 10, 58, 27, TimeSpan.FromHours(2)));
+                var fetchOptions = new FetchOptions
+                                       {
+                                           CredentialsProvider =
+                                               (url, user, cred) =>
+                                               new UsernamePasswordCredentials
+                                                   {
+                                                       Username = gitUser, 
+                                                       Password = gitPass
+                                                   }
+                                       };
+                var mergeOptions = new MergeOptions();
+                var pullOptions = new PullOptions { FetchOptions = fetchOptions, MergeOptions = mergeOptions };
+                this.gitRepo.Network.Pull(signature, pullOptions);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// The commit.
+        /// </summary>
+        /// <param name="fileToCommit">
+        /// The file to commit.
+        /// </param>
+        /// <returns>
+        /// Result in boolean <see cref="bool"/>.
+        /// </returns>
+        public bool Commit(string fileToCommit = null)
+        {
+            // Stage the file
+            if (fileToCommit != null)
+            {
+                this.gitRepo.Stage(fileToCommit);
+            }
+
+            // Commit
+            try
+            {
+                this.gitRepo.Commit(
+                    "password changes", 
+                    new Signature("pass4win", "pass4win", DateTimeOffset.Now), 
+                    new Signature("pass4win", "pass4win", DateTimeOffset.Now));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Push function.
+        /// </summary>
+        /// <param name="gitUser">
+        /// The git user.
+        /// </param>
+        /// <param name="gitPass">
+        /// The git pass.
+        /// </param>
+        /// <returns>
+        /// Result in boolean <see cref="bool"/>.
+        /// </returns>
+        public bool Push(string gitUser, string gitPass)
+        {
+            var tc = this.gitRepo.Diff.Compare<TreeChanges>(
+                            this.gitRepo.Branches["origin/master"].Tip.Tree,
+                this.gitRepo.Head.Tip.Tree);
+            if (!tc.Any())
+            {
+                return true;
+            }
+
+            var remote = this.gitRepo.Network.Remotes["origin"];
+            var options = new PushOptions
+                              {
+                                  CredentialsProvider =
+                                      (url, user, cred) =>
+                                      new UsernamePasswordCredentials
+                                          {
+                                              Username = gitUser, 
+                                              Password = gitPass
+                                          }
+                              };
+            var pushRefSpec = @"refs/heads/master";
+            try
+            {
+                this.gitRepo.Network.Push(remote, pushRefSpec, options);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Removes a file from the Git repo
+        /// </summary>
+        /// <param name="removeFile">
+        /// The filename.
+        /// </param>
+        /// <returns>
+        /// result in boolean<see cref="bool"/>.
+        /// </returns>
+        public bool RemoveFile(string removeFile)
+        {
+            try
+            {
+                this.gitRepo.Remove(removeFile);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Free the resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// The disposing.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.gitRepo.Dispose();
+            }
+        }
+    }
+}
---
+++ Pass4Win/pgp.cs
@@ -0,0 +1,415 @@
+﻿/*
+ * Copyright (C) 2105 by Mike Bos
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * A copy of the license is obtainable at http://www.gnu.org/licenses/gpl-3.0.en.html#content
+*/
+
+namespace Pass4Win
+{
+    using System;
+    using System.IO;
+    using Org.BouncyCastle.Bcpg.OpenPgp;
+    using Org.BouncyCastle.Utilities.IO;
+    using Org.BouncyCastle.Bcpg;
+    using Org.BouncyCastle.Security;
+
+    public sealed class PgpHandling
+    {
+        public PgpHandling()
+        {
+            
+        }
+
+        private static void DecryptFile(
+            string inputFileName,
+            string keyFileName,
+            char[] passwd,
+            string defaultFileName)
+        {
+            using (Stream input = File.OpenRead(inputFileName),
+                   keyIn = File.OpenRead(keyFileName))
+            {
+                DecryptFile(input, keyIn, passwd, defaultFileName);
+            }
+        }
+
+        /**
+		 * decrypt the passed in message stream
+		 */
+        private static void DecryptFile(
+            Stream inputStream,
+            Stream keyIn,
+            char[] passwd,
+            string defaultFileName)
+        {
+            inputStream = PgpUtilities.GetDecoderStream(inputStream);
+
+            try
+            {
+                PgpObjectFactory pgpF = new PgpObjectFactory(inputStream);
+                PgpEncryptedDataList enc;
+
+                PgpObject o = pgpF.NextPgpObject();
+                //
+                // the first object might be a PGP marker packet.
+                //
+                if (o is PgpEncryptedDataList)
+                {
+                    enc = (PgpEncryptedDataList)o;
+                }
+                else
+                {
+                    enc = (PgpEncryptedDataList)pgpF.NextPgpObject();
+                }
+
+                //
+                // find the secret key
+                //
+                PgpPrivateKey sKey = null;
+                PgpPublicKeyEncryptedData pbe = null;
+                PgpSecretKeyRingBundle pgpSec = new PgpSecretKeyRingBundle(
+                    PgpUtilities.GetDecoderStream(keyIn));
+
+                foreach (PgpPublicKeyEncryptedData pked in enc.GetEncryptedDataObjects())
+                {
+                    sKey = FindSecretKey(pgpSec, pked.KeyId, passwd);
+
+                    if (sKey != null)
+                    {
+                        pbe = pked;
+                        break;
+                    }
+                }
+
+                if (sKey == null)
+                {
+                    throw new ArgumentException("secret key for message not found.");
+                }
+
+                Stream clear = pbe.GetDataStream(sKey);
+
+                PgpObjectFactory plainFact = new PgpObjectFactory(clear);
+
+                PgpObject message = plainFact.NextPgpObject();
+
+                if (message is PgpCompressedData)
+                {
+                    PgpCompressedData cData = (PgpCompressedData)message;
+                    PgpObjectFactory pgpFact = new PgpObjectFactory(cData.GetDataStream());
+
+                    message = pgpFact.NextPgpObject();
+                }
+
+                if (message is PgpLiteralData)
+                {
+                    PgpLiteralData ld = (PgpLiteralData)message;
+
+                    string outFileName = ld.FileName;
+                    if (outFileName.Length == 0)
+                    {
+                        outFileName = defaultFileName;
+                    }
+
+                    Stream fOut = File.Create(outFileName);
+                    Stream unc = ld.GetInputStream();
+                    Streams.PipeAll(unc, fOut);
+                    fOut.Close();
+                }
+                else if (message is PgpOnePassSignatureList)
+                {
+                    throw new PgpException("encrypted message contains a signed message - not literal data.");
+                }
+                else
+                {
+                    throw new PgpException("message is not a simple encrypted file - type unknown.");
+                }
+
+                if (pbe.IsIntegrityProtected())
+                {
+                    if (!pbe.Verify())
+                    {
+                        Console.Error.WriteLine("message failed integrity check");
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("message integrity check passed");
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("no message integrity check");
+                }
+            }
+            catch (PgpException e)
+            {
+                Console.Error.WriteLine(e);
+
+                Exception underlyingException = e.InnerException;
+                if (underlyingException != null)
+                {
+                    Console.Error.WriteLine(underlyingException.Message);
+                    Console.Error.WriteLine(underlyingException.StackTrace);
+                }
+            }
+        }
+
+        private static void EncryptFile(
+            string outputFileName,
+            string inputFileName,
+            string encKeyFileName,
+            bool armor,
+            bool withIntegrityCheck)
+        {
+            PgpPublicKey encKey = ReadPublicKey(encKeyFileName);
+
+            using (Stream output = File.Create(outputFileName))
+            {
+                EncryptFile(output, inputFileName, encKey, armor, withIntegrityCheck);
+            }
+        }
+
+        private static void EncryptFile(
+            Stream outputStream,
+            string fileName,
+            PgpPublicKey encKey,
+            bool armor,
+            bool withIntegrityCheck)
+        {
+            if (armor)
+            {
+                outputStream = new ArmoredOutputStream(outputStream);
+            }
+
+            try
+            {
+                byte[] bytes = CompressFile(fileName, CompressionAlgorithmTag.Zip);
+
+                PgpEncryptedDataGenerator encGen = new PgpEncryptedDataGenerator(
+                    SymmetricKeyAlgorithmTag.Cast5, withIntegrityCheck, new SecureRandom());
+                encGen.AddMethod(encKey);
+
+                Stream cOut = encGen.Open(outputStream, bytes.Length);
+
+                cOut.Write(bytes, 0, bytes.Length);
+                cOut.Close();
+
+                if (armor)
+                {
+                    outputStream.Close();
+                }
+            }
+            catch (PgpException e)
+            {
+                Console.Error.WriteLine(e);
+
+                Exception underlyingException = e.InnerException;
+                if (underlyingException != null)
+                {
+                    Console.Error.WriteLine(underlyingException.Message);
+                    Console.Error.WriteLine(underlyingException.StackTrace);
+                }
+            }
+        }
+
+        public static byte[] Decrypt(byte[] encrypted, char[] passPhrase)
+        {
+            Stream inputStream = new MemoryStream(encrypted);
+
+            inputStream = PgpUtilities.GetDecoderStream(inputStream);
+
+            PgpObjectFactory pgpF = new PgpObjectFactory(inputStream);
+            PgpEncryptedDataList enc = null;
+            PgpObject o = pgpF.NextPgpObject();
+
+            //
+            // the first object might be a PGP marker packet.
+            //
+            if (o is PgpEncryptedDataList)
+            {
+                enc = (PgpEncryptedDataList)o;
+            }
+            else
+            {
+                enc = (PgpEncryptedDataList)pgpF.NextPgpObject();
+            }
+
+            PgpPbeEncryptedData pbe = (PgpPbeEncryptedData)enc[0];
+
+            Stream clear = pbe.GetDataStream(passPhrase);
+
+            PgpObjectFactory pgpFact = new PgpObjectFactory(clear);
+
+            PgpCompressedData cData = (PgpCompressedData)pgpFact.NextPgpObject();
+
+            pgpFact = new PgpObjectFactory(cData.GetDataStream());
+
+            PgpLiteralData ld = (PgpLiteralData)pgpFact.NextPgpObject();
+
+            Stream unc = ld.GetInputStream();
+
+            return Streams.ReadAll(unc);
+        }
+
+        /**
+        * Simple PGP encryptor between byte[].
+        *
+        * @param clearData  The test to be encrypted
+        * @param passPhrase The pass phrase (key).  This method assumes that the
+        *                   key is a simple pass phrase, and does not yet support
+        *                   RSA or more sophisiticated keying.
+        * @param fileName   File name. This is used in the Literal Data Packet (tag 11)
+        *                   which is really inly important if the data is to be
+        *                   related to a file to be recovered later.  Because this
+        *                   routine does not know the source of the information, the
+        *                   caller can set something here for file name use that
+        *                   will be carried.  If this routine is being used to
+        *                   encrypt SOAP MIME bodies, for example, use the file name from the
+        *                   MIME type, if applicable. Or anything else appropriate.
+        *
+        * @param armor
+        *
+        * @return encrypted data.
+        * @exception IOException
+        * @exception PgpException
+        */
+        public static byte[] Encrypt(byte[] clearData, char[] passPhrase, string fileName, SymmetricKeyAlgorithmTag algorithm, bool armor)
+        {
+            if (fileName == null)
+            {
+                fileName = PgpLiteralData.Console;
+            }
+
+            byte[] compressedData = Compress(clearData, fileName, CompressionAlgorithmTag.Zip);
+
+            MemoryStream bOut = new MemoryStream();
+
+            Stream output = bOut;
+            if (armor)
+            {
+                output = new ArmoredOutputStream(output);
+            }
+
+            PgpEncryptedDataGenerator encGen = new PgpEncryptedDataGenerator(algorithm, new SecureRandom());
+            encGen.AddMethod(passPhrase);
+
+            Stream encOut = encGen.Open(output, compressedData.Length);
+
+            encOut.Write(compressedData, 0, compressedData.Length);
+            encOut.Close();
+
+            if (armor)
+            {
+                output.Close();
+            }
+
+            return bOut.ToArray();
+        }
+
+        private static byte[] Compress(byte[] clearData, string fileName, CompressionAlgorithmTag algorithm)
+        {
+            MemoryStream bOut = new MemoryStream();
+
+            PgpCompressedDataGenerator comData = new PgpCompressedDataGenerator(algorithm);
+            Stream cos = comData.Open(bOut); // open it with the final destination
+            PgpLiteralDataGenerator lData = new PgpLiteralDataGenerator();
+
+            // we want to Generate compressed data. This might be a user option later,
+            // in which case we would pass in bOut.
+            Stream pOut = lData.Open(
+                cos,					// the compressed output stream
+                PgpLiteralData.Binary,
+                fileName,				// "filename" to store
+                clearData.Length,		// length of clear data
+                DateTime.UtcNow			// current time
+            );
+
+            pOut.Write(clearData, 0, clearData.Length);
+            pOut.Close();
+
+            comData.Close();
+
+            return bOut.ToArray();
+        }
+
+        /**
+ * Search a secret key ring collection for a secret key corresponding to keyID if it
+ * exists.
+ * 
+ * @param pgpSec a secret key ring collection.
+ * @param keyID keyID we want.
+ * @param pass passphrase to decrypt secret key with.
+ * @return
+ * @throws PGPException
+ * @throws NoSuchProviderException
+ */
+        internal static PgpPrivateKey FindSecretKey(PgpSecretKeyRingBundle pgpSec, long keyID, char[] pass)
+        {
+            PgpSecretKey pgpSecKey = pgpSec.GetSecretKey(keyID);
+
+            if (pgpSecKey == null)
+            {
+                return null;
+            }
+
+            return pgpSecKey.ExtractPrivateKey(pass);
+        }
+
+        internal static PgpPublicKey ReadPublicKey(string fileName)
+        {
+            using (Stream keyIn = File.OpenRead(fileName))
+            {
+                return ReadPublicKey(keyIn);
+            }
+        }
+
+        /**
+		 * A simple routine that opens a key ring file and loads the first available key
+		 * suitable for encryption.
+		 * 
+		 * @param input
+		 * @return
+		 * @throws IOException
+		 * @throws PGPException
+		 */
+        internal static PgpPublicKey ReadPublicKey(Stream input)
+        {
+            PgpPublicKeyRingBundle pgpPub = new PgpPublicKeyRingBundle(
+                PgpUtilities.GetDecoderStream(input));
+
+            //
+            // we just loop through the collection till we find a key suitable for encryption, in the real
+            // world you would probably want to be a bit smarter about this.
+            //
+
+            foreach (PgpPublicKeyRing keyRing in pgpPub.GetKeyRings())
+            {
+                foreach (PgpPublicKey key in keyRing.GetPublicKeys())
+                {
+                    if (key.IsEncryptionKey)
+                    {
+                        return key;
+                    }
+                }
+            }
+
+            throw new ArgumentException("Can't find encryption key in key ring.");
+        }
+
+
+        internal static byte[] CompressFile(string fileName, CompressionAlgorithmTag algorithm)
+        {
+            MemoryStream bOut = new MemoryStream();
+            PgpCompressedDataGenerator comData = new PgpCompressedDataGenerator(algorithm);
+            PgpUtilities.WriteFileToLiteralData(comData.Open(bOut), PgpLiteralData.Binary,
+                new FileInfo(fileName));
+            comData.Close();
+            return bOut.ToArray();
+        }
+    }
+}
