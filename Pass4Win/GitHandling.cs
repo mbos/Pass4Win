090e274f4e4e425a9339a77fa9abc9522d76887e
Mike Bos <mikebos@mik3.com>
2015-12-28 21:00:14 +0100

    Updated the class for use with an external git

--- Pass4Win/GitHandling.cs
+++ Pass4Win/GitHandling.cs
@@ -14,6 +14,9 @@ namespace Pass4Win
     using System.Net.Sockets;
 
     using LibGit2Sharp;
+    using System.IO;
+    using System.Diagnostics;
+    using System.Text.RegularExpressions;
 
     /// <summary>
     ///     Class to interface with the Git Repo
@@ -34,6 +37,37 @@ namespace Pass4Win
         ///     The repo location.
         /// </summary>
         private readonly string repoLocation;
+
+        /// <summary>
+        ///     External or internal GIT.
+        ///     When filled in it's external
+        /// </summary>
+        private readonly string ExternalGitPath;
+        
+        /// <summary>
+        ///     Connection string when using external git 
+        /// </summary>
+        private readonly string ConnectionString;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GitHandling"/> class.
+        /// </summary>
+        /// <param name="repoLocation">
+        /// The repo location.
+        /// </param>
+        /// <param name="host">
+        /// The host.
+        /// </param>
+        /// <param name="GitLocation">
+        /// The full path of the Git executable.
+        /// </param>
+        public GitHandling(string repoLocation, string host, string GitLocation, string ConnectionString)
+        {
+            this.repoLocation = repoLocation;
+            this.host = host;
+            this.ExternalGitPath = GitLocation;
+            this.ConnectionString = ConnectionString;
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GitHandling"/> class.
@@ -89,10 +123,12 @@ namespace Pass4Win
             {
                 var client = new TcpClient();
                 var client2 = new TcpClient();
+                var client3 = new TcpClient();
                 try
                 {
                     var gitAlive = true;
                     var httpsAlive = true;
+                    var SSHAlive = true;
 
                     // Check Git connection
                     var result = client.BeginConnect(hostTest.Authority, 9418, null, null);
@@ -112,7 +148,16 @@ namespace Pass4Win
                         httpsAlive = false;
                     }
 
-                    if (gitAlive || httpsAlive)
+                    // Check SSH
+                    var result3 = client3.BeginConnect(hostTest.Authority, 22, null, null);
+                    result3.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+                    if (!client3.Connected)
+                    {
+                        client3.Close();
+                        SSHAlive = false;
+                    }
+
+                    if (gitAlive || httpsAlive || SSHAlive)
                     {
                         return true;
                     }
@@ -137,21 +182,64 @@ namespace Pass4Win
         {
             if (CheckConnection(this.host) && Repository.IsValid(this.repoLocation))
             {
-                try
+                if (File.Exists(ExternalGitPath))
                 {
-                    this.gitRepo = new Repository(this.repoLocation);
-
-                    // Making sure core.autocrlf = true
-                    this.gitRepo.Config.Set("core.autocrlf", true);
-                    return true;
+                    // nothing to do here as yet
                 }
-                catch (Exception)
+                else
                 {
-                    return false;
+                    try
+                    {
+                        this.gitRepo = new Repository(this.repoLocation);
+
+                        // Making sure core.autocrlf = true
+                        this.gitRepo.Config.Set("core.autocrlf", true);
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
                 }
             }
-
             return false;
+        }
+
+        private string ExecuteGitCommand(string command)
+        {
+            ProcessStartInfo gitInfo = new ProcessStartInfo();
+            string output = "";
+            string error = string.Empty;
+
+            gitInfo.CreateNoWindow = true;
+            gitInfo.RedirectStandardError = true;
+            gitInfo.RedirectStandardOutput = true;
+            gitInfo.UseShellExecute = false;
+            gitInfo.FileName = this.ExternalGitPath;
+
+            Process gitProcess = new Process();
+            gitInfo.Arguments = command;
+            gitInfo.WorkingDirectory = this.repoLocation;
+
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+
+            using (System.IO.StreamReader myOutput = gitProcess.StandardOutput)
+            {
+                output = myOutput.ReadToEnd();
+            }
+            using (System.IO.StreamReader myError = gitProcess.StandardError)
+            {
+                error = myError.ReadToEnd();
+
+            }
+
+            gitProcess.WaitForExit();
+            gitProcess.Close();
+
+            // give the output or the error message
+            if (error.Length == 0) return output;
+            else return error;
         }
 
         /// <summary>
@@ -168,27 +256,38 @@ namespace Pass4Win
         /// </returns>
         public bool GitClone(string username, string password)
         {
-            try
+            if (File.Exists(ExternalGitPath))
             {
-                Repository.Clone(
-                    this.host, 
-                    this.repoLocation, 
-                    new CloneOptions
+                string GitOutput = ExecuteGitCommand("clone " + ConnectionString + " " + repoLocation);
+                bool result = Regex.IsMatch(GitOutput, "\\bfatal\\b", RegexOptions.IgnoreCase);
+                if (!result)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    Repository.Clone(
+                        this.host,
+                        this.repoLocation,
+                        new CloneOptions
                         {
                             CredentialsProvider =
-                                (url, user, cred) =>
-                                new UsernamePasswordCredentials
+                                    (url, user, cred) =>
+                                    new UsernamePasswordCredentials
                                     {
-                                        Username = username, 
+                                        Username = username,
                                         Password = password
                                     }
                         });
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
-            catch (Exception)
-            {
-                return false;
-            }
-
             return true;
         }
 
@@ -206,31 +305,42 @@ namespace Pass4Win
         /// </returns>
         public bool Fetch(string gitUser, string gitPass)
         {
-            try
+            if (File.Exists(ExternalGitPath))
             {
-                var signature = new Signature(
-                    "pass4win", 
-                    "pull@pass4win.com", 
-                    new DateTimeOffset(2011, 06, 16, 10, 58, 27, TimeSpan.FromHours(2)));
-                var fetchOptions = new FetchOptions
-                                       {
-                                           CredentialsProvider =
-                                               (url, user, cred) =>
-                                               new UsernamePasswordCredentials
+                string GitOutput = ExecuteGitCommand("pull");
+                bool result = Regex.IsMatch(GitOutput, "\\bfatal\\b", RegexOptions.IgnoreCase);
+                if (!result)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    var signature = new Signature(
+                        "pass4win",
+                        "pull@pass4win.com",
+                        new DateTimeOffset(2011, 06, 16, 10, 58, 27, TimeSpan.FromHours(2)));
+                    var fetchOptions = new FetchOptions
+                    {
+                        CredentialsProvider =
+                                                   (url, user, cred) =>
+                                                   new UsernamePasswordCredentials
                                                    {
-                                                       Username = gitUser, 
+                                                       Username = gitUser,
                                                        Password = gitPass
                                                    }
-                                       };
-                var mergeOptions = new MergeOptions();
-                var pullOptions = new PullOptions { FetchOptions = fetchOptions, MergeOptions = mergeOptions };
-                this.gitRepo.Network.Pull(signature, pullOptions);
+                    };
+                    var mergeOptions = new MergeOptions();
+                    var pullOptions = new PullOptions { FetchOptions = fetchOptions, MergeOptions = mergeOptions };
+                    this.gitRepo.Network.Pull(signature, pullOptions);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
-            catch (Exception)
-            {
-                return false;
-            }
-
             return true;
         }
 
@@ -248,13 +358,25 @@ namespace Pass4Win
             // Stage the file
             if (fileToCommit != null)
             {
-                try
+                if (File.Exists(ExternalGitPath))
                 {
-                    this.gitRepo.Stage(fileToCommit);
+                    string GitOutput = ExecuteGitCommand("git add" + fileToCommit);
+                    bool result = Regex.IsMatch(GitOutput, "\\bfatal\\b", RegexOptions.IgnoreCase);
+                    if (!result)
+                    {
+                        return false;
+                    }
                 }
-                catch (Exception)
+                else
                 {
-                    // file most likely already staged. We'll just ignore the error.
+                    try
+                    {
+                        this.gitRepo.Stage(fileToCommit);
+                    }
+                    catch (Exception)
+                    {
+                        // file most likely already staged. We'll just ignore the error.
+                    }
                 }
             }
 
@@ -296,25 +418,38 @@ namespace Pass4Win
                 return true;
             }
 
-            var remote = this.gitRepo.Network.Remotes["origin"];
-            var options = new PushOptions
-                              {
-                                  CredentialsProvider =
-                                      (url, user, cred) =>
-                                      new UsernamePasswordCredentials
+            if (File.Exists(ExternalGitPath))
+            {
+                string GitOutput = ExecuteGitCommand("git commit -m Pass4Win_commit");
+                bool result = Regex.IsMatch(GitOutput, "\\bfatal\\b", RegexOptions.IgnoreCase);
+                if (!result)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+
+                var remote = this.gitRepo.Network.Remotes["origin"];
+                var options = new PushOptions
+                {
+                    CredentialsProvider =
+                                          (url, user, cred) =>
+                                          new UsernamePasswordCredentials
                                           {
-                                              Username = gitUser, 
+                                              Username = gitUser,
                                               Password = gitPass
                                           }
-                              };
-            var pushRefSpec = @"refs/heads/master";
-            try
-            {
-                this.gitRepo.Network.Push(remote, pushRefSpec, options);
-            }
-            catch (Exception)
-            {
-                return false;
+                };
+                var pushRefSpec = @"refs/heads/master";
+                try
+                {
+                    this.gitRepo.Network.Push(remote, pushRefSpec, options);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
 
             return true;
@@ -331,15 +466,26 @@ namespace Pass4Win
         /// </returns>
         public bool RemoveFile(string removeFile)
         {
-            try
+            if (File.Exists(ExternalGitPath))
             {
-                this.gitRepo.Remove(removeFile);
+                string GitOutput = ExecuteGitCommand("git rm " + removeFile);
+                bool result = Regex.IsMatch(GitOutput, "\\bfatal\\b", RegexOptions.IgnoreCase);
+                if (!result)
+                {
+                    return false;
+                }
             }
-            catch (Exception)
+            else
             {
-                return false;
+                try
+                {
+                    this.gitRepo.Remove(removeFile);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
-
             return true;
         }
 
