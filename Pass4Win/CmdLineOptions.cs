ddc7e0e26ea14f69c2e65aa369adc6f95fa0dff4
Mike Bos <mikebos@mik3.com>
2016-07-14 09:36:32 +0200

    Command line parsing handling made scalable

---
+++ Pass4Win/CmdLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+
+namespace Pass4Win
+{
+    class CmdLineOptions
+    {
+        [Option('d', "debug", DefaultValue = false, HelpText = "Set the log file to your personal directory")]
+        public bool Debug { get; set; }
+
+        [Option('r', "reset", DefaultValue = false, HelpText = "Resets the config file")]
+        public bool Reset { get; set; }
+
+        [Option('g', "git", DefaultValue = true, HelpText = "Use git.")]
+        public bool UseGit { get; set; }
+
+    }
+}
--- Pass4Win/Program.cs
+++ Pass4Win/Program.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using log4net.Config;
 using System.IO;
 using System.IO.IsolatedStorage;
+using CommandLine;
+using CommandLine.Text;
 
 namespace Pass4Win
 {
@@ -23,16 +25,39 @@ namespace Pass4Win
             // parsing command line
             string[] args = Environment.GetCommandLineArgs();
             string PersonalFolder = Path.GetTempPath() + "Pass4Win.log";
+            /*
+            // Set logfile location to personal directory
             if (args.Length > 1 && args[1].Equals("debug"))
             {
                 PersonalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\Pass4Win.log";
             }
+            // Delete config file
             if (args.Length > 1 && args[1].Equals("resetconfig"))
             {
                 IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
                 isoStore.DeleteFile("Pass4Win.json");
             }
-            
+            */
+
+            var options = new CmdLineOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // consume Options instance properties
+                if (options.Debug)
+                {
+                    PersonalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\Pass4Win.log";
+                }
+                if (options.Reset)
+                {
+                    IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+                    isoStore.DeleteFile("Pass4Win.json");
+                }
+                if (!options.UseGit)
+                {
+                    // do stuff
+                }
+            }
+
             // setting up logging
 
             Environment.SetEnvironmentVariable("log4netFileName", PersonalFolder + "\\Pass4Win.log");
